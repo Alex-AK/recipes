ab6b61463baba87f95389c9b5bdf37df8cb08fe1
Kingaa6 <Kingaa6@gmail.com>
2019-02-21 11:26:36 -0800

    File setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+// const _Router = require('./_');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
+
+// server.use('/api/_', _Router);
+
+server.get('/test', (req, res) => {
+  db.get()
+    .then(res => res.status(200).json({ success: true, res }))
+    .catch(err =>
+      res.status(500).json({
+        success: false,
+        message: 'Unable to retrieve actions. Please try again.'
+      })
+    );
+});
+
+server.listen(4000, () => {
+  console.log('*** Listening on port 4000 ***');
+});

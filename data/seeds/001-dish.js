82ee8e0152defe796ddb1aafdbf46320fe8b9be2
Kingaa6 <Kingaa6@gmail.com>
2019-02-21 15:24:06 -0800

    Add seeds

--- data/seeds/001-dish.js
+++ data/seeds/001-dish.js
@@ -11,10 +11,3 @@ exports.seed = function(knex, Promise) {
       ]);
     });
 };
-
-// recipe id 3
-// recipe -> recipe name, add ingredient id 1, quantity 4g, add ingredient id 2, quantity 2g
-
-// ingredient id 1 salt
-// ingredient 2 sugar
-// ingredient 3 flour
